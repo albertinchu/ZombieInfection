37df7ec6f855e0e0d724c85a6ce7f50ff826ce5c
Albertinchu <49210963+albertinchu@users.noreply.github.com>
2019-04-15 01:59:58 +0200

    Add files via upload

---
+++ Zi/PlayerEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using scp4aiur;
+namespace Zi
+{
+    partial class PlayerEvents : IEventHandlerPlayerDie
+    {
+        private Zi plugin;
+        public PlayerEvents(Zi plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnPlayerDie(PlayerDeathEvent ev)
+        {
+            if((ev.Killer.TeamRole.Role == Role.SCP_049_2)&&(ev.Killer.SteamId != ev.Player.SteamId))
+            {
+                ev.SpawnRagdoll = false;
+                Timing.Run(Zi(ev.Player));               
+            }
+        }
+
+        public static IEnumerable<float> Zi(Player player)
+        {                  
+                yield return 0.2f;
+            player.ChangeRole(Role.SCP_049_2);            
+        }
+    }
+}
---
+++ Zi/Zi.cs
@@ -0,0 +1,41 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using scp4aiur;
+namespace Zi
+{
+
+    [PluginDetails(
+        author = "Albertinchu",
+        name = "Zi",
+        description = "si un Zombie mata a un jugador este se transforma en zombie.",
+        id = "albertinchu.zi",
+        version = "1.0.0",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class Zi : Plugin
+    {
+
+        public override void OnDisable()
+        {
+            this.Info("Zi - Desactivado");
+        }
+
+        public override void OnEnable()
+        {
+            Info("Zi - activado.");
+        }
+
+        public override void Register()
+        { 
+            Timing.Init(this);
+            this.AddEventHandlers(new PlayerEvents(this));
+        }
+        public void RefreshConfig()
+        {
+        }
+    }
+
+}
+
